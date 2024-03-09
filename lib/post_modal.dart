61d0c8d51bef0271cba02b36ec7ef1e4de7c3268
Princilia <princilia.dabre02@gmail.com>
2024-03-09 14:58:04 +0530

    first commit

---
+++ lib/post_modal.dart
@@ -0,0 +1,28 @@
+class Posts {
+  int? userId;
+  int? id;
+  String? title;
+  String? body;
+
+  Posts({this.userId, this.id, this.title, this.body});
+
+  Posts.fromJson(Map<String, dynamic> json) {
+    userId = json["userId"];
+    id = json["id"];
+    title = json["title"];
+    body = json["body"];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> _data = <String, dynamic>{};
+    _data["userId"] = userId;
+    _data["id"] = id;
+    _data["title"] = title;
+    _data["body"] = body;
+    return _data;
+  }
+
+  static List<Posts> getPostData(List<dynamic> jsonList) {
+    return jsonList.map((json) => Posts.fromJson(json)).toList();
+  }
+}
---
+++ lib/screen1.dart
@@ -0,0 +1,87 @@
+import 'dart:convert';
+
+import 'package:api_practice/post_modal.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class screen1 extends StatefulWidget {
+  const screen1({super.key});
+
+  @override
+  State<screen1> createState() => _screen1State();
+}
+
+class _screen1State extends State<screen1> {
+  //List? ofUser = [];
+  List<Posts>? ofUser;
+  void getPost() async {
+    try {
+      var post = await http
+          .get(Uri.parse("https://jsonplaceholder.typicode.com/posts"));
+      if (post.statusCode == 200) {
+        ofUser = Posts.getPostData(jsonDecode(post.body));
+        setState(() {});
+      } else {
+        print(post.statusCode);
+      }
+    } catch (e) {}
+  }
+
+  @override
+  void initState() {
+    getPost();
+    // TODO: implement initState
+    super.initState();
+  }
+
+  Widget buildCard(Posts post) {
+    return Card(
+      margin: const EdgeInsets.all(20),
+      elevation: 5,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+          side: const BorderSide(color: Colors.grey)),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              "Title",
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            ),
+            Text(post.title ?? ""),
+            Divider(),
+            Text(
+              "Description",
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            ),
+            Text(post.body.toString()),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: Center(child: Text("TITLE")),
+      ),
+      body: Column(children: [
+        Expanded(
+          child: ListView.builder(
+            scrollDirection: Axis.vertical,
+            itemCount: ofUser!.length,
+            itemBuilder: (context, index) {
+              return buildCard(ofUser![index]);
+            },
+          ),
+        ),
+      ]),
+    );
+  }
+}
